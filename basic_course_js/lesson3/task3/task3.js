32524e6229d695c6f2ec117ee03b393354d7a3ff
brykin.ilia@gmail.com <ilia.brykin@prounix.de>
2021-12-27 20:54:08 +0100

    JS: lesson3

---
+++ basic_course_js/lesson3/task3/task3.js
@@ -0,0 +1,23 @@
+// Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя
+// метод forEach.
+"use strict";
+
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+products.forEach(product => {
+  product.price *= 0.85;
+});
+console.log("products", products);
